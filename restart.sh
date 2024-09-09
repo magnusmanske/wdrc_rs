4132aff0072ddab621cd4518d18e3028243c8d87
Magnus Manske <magnusmanske@googlemail.com>
2024-09-09 16:02:29 +0100

    scripts

---
+++ restart.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+toolforge jobs delete rustbot
+touch ~/rustbot.out
+mv ~/rustbot.out ~/rustbot_out.bak
+toolforge jobs run --mem 2000Mi --cpu 1 --continuous --mount=all \
+	--image tool-wdrc/tool-wdrc:latest \
+	--command "sh -c 'target/release/wdrc bot /data/project/wdrc/wdrc_rs/config.json'" \
+	--filelog -o /data/project/wdrc/rustbot.out -e /data/project/wdrc/rustbot.err \
+	rustbot
